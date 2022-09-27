e2c35b2efdc796e8881d571a801f9cebeca66a94
rushikeshpatilcv <rushikesh.patil@clairvoyantsoft.com>
2022-09-27 11:45:23 +0530

    DM-4 changes to integrate document list api

--- src/components/login/SetNewPassword.js
+++ src/components/login/SetNewPassword.js
@@ -1,63 +1,56 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
 import { useNavigate, useLocation } from "react-router-dom";
 
-function SetNewPassword({props}){
+function SetNewPassword(props) {
   const [password, setPassword] = useState("");
-  let navigate = useNavigate();
-  const { state } = useLocation();
-  const userObj = state.userObj;
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const oldPassword = location.state.password
+  const username = location.state.username
 
   const handleInputChange = (e) => {
-    const {id, value} = e.target;
-        if(id === "password"){
-            setPassword(value);
-        }
-      }
+    const { id, value } = e.target;
+    if (id === "password") {
+      setPassword(value);
+    }
+  }
   const goToDocumentPage = () => {
     navigate("/document");
   };
 
-  async function SetNewPassword() {
+  async function setNewPassword() {
     try {
-        Auth.completeNewPassword(userObj, password).then(user => {
-          // at this time the user is logged in if no MFA required
-          console.log(userObj);
-          goToDocumentPage();
-        }).catch(e => {
-          console.log(e);
-        });
-      
-    } catch (error) {
-        console.log('error confirming sign up', error);
+      Auth.signIn(username, oldPassword)
+        .then(user => {
+          Auth.completeNewPassword(user, password).then(user => {
+            goToDocumentPage();
+          }).catch(e => {
+            console.log(e);
+          });
+
+        })
+      } catch (error) {
+          console.log('error confirming sign up', error);
+        }
+
     }
-  }
 
-  async function handleSignOut(){    // You can pass an object which has the username, password and validationData which is sent to a PreAuthentication Lambda trigger
-    Auth.signOut()
-      .then(user => {
-        console.log(user);
-      })
-      .catch(err => console.log(err));
-  };
-
-
-    return (
+  return (
+    <div>
+      <label>Enter new password</label>
+      <input
+        type="text"
+        placeholder="Enter password"
+        id="password"
+        onChange={(e) => handleInputChange(e)}
+      />
       <div>
-            <label>Enter new password</label>
-            <input
-              type="text"
-              placeholder="Enter password"
-              id="password"
-              onChange = {(e) => handleInputChange(e)}
-            />
-          <div>
-        </div>
-        <button onClick={()=>SetNewPassword()} type="submit" className="btn">Confirm</button>
-        <button onClick={()=>handleSignOut()} type="submit" className="btn">SignOut</button>
-        </div>
-      
-    );
+      </div>
+      <button onClick={() => setNewPassword()} type="submit" className="btn">Confirm</button>
+    </div>
+
+  );
 }
 export default SetNewPassword;
